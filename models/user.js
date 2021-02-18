8384ccc4d93c6b7afe1cef2ae769f21477eda1e5
tommytoyoutoo <tommytoyou@gmail.com>
2021-02-17 19:38:59 -0600

    more associations

--- models/user.js
+++ models/user.js
@@ -12,6 +12,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.user.hasMany(models.post)
+      models.user.hasMany(models.comment)
     }
   };
   user.init({

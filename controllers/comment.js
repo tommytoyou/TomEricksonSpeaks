3b5e4ac63f947876650d209a97b11fe8b45566e0
tommytoyoutoo <tommytoyou@gmail.com>
2021-02-17 18:39:56 -0600

    comment post route

---
+++ controllers/comment.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+
+router.post('/', async function(req, res) {
+    try{
+        let comment = await db.comment.create(req.body)
+        res.redirect(`/posts/${comment.postId}`)
+    }catch(error) {
+        console.log(error.message)
+        res.status(400).render('main/404')
+    }
+})
